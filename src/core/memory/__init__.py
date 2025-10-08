a4ad8ebb7827b792e138793f04ae1f45e6f6e729
eraevous <Rhodesz87@gmail.com>
2025-10-08 15:50:15 -0500

    Delete src directory
    Experimental testing

--- src/core/memory/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Public API for the :mod:`core.memory` package."""
-
-from .frame_store import FrameStore
-
-__all__ = ["FrameStore"]
--- src/core/synthesis/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Public API for the :mod:`core.synthesis` package."""
-
-from .summarizer import summarize_documents
-
-__all__ = ["summarize_documents"]
